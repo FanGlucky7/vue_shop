8d64a0bf06b5d29001b92c9625b8316183c7557e
FanG <renyifang@zju.edu.cn>
2020-07-18 16:34:25 +0800

    完成了权限功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '.././components/Login.vue'
 import Home from '.././components/Home.vue'
 import Welcome from '.././components/Welcome.vue'
 import Users from '.././components/user/Users.vue'
+import Rights from '.././components/power/Rights.vue'
+import Roles from '.././components/power/Roles.vue'
 
 Vue.use(VueRouter)
 
@@ -16,22 +18,24 @@ const routes = [
         redirect: '/welcome',
         children: [
             { path: '/welcome', component: Welcome },
-            { path: '/users', component: Users }
+            { path: '/users', component: Users },
+            { path: '/rights', component: Rights },
+            { path: '/roles', component: Roles }
         ]
     }
 ]
 const router = new VueRouter({ routes })
 
-//挂在路由导航守卫
+// 挂在路由导航守卫
 router.beforeEach((to, from, next) => {
     if (to.path === '/login') return next();
-    //获取token
+    // 获取token
     const tokenStr = window.sessionStorage.getItem('token')
     if (!tokenStr) return next('/login')
     next()
 })
 
-//用element ui写得侧边导航栏中用到了路由，然后重复点击这个路由出现错误
+// 用element ui写得侧边导航栏中用到了路由，然后重复点击这个路由出现错误
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
     return originalPush.call(this, location).catch(err => err)
